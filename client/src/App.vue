01270ed983b992aae8b3b4b49fe6752dba51ff79
NgoMauTruongQB <truongngo2707@gmail.com>
2023-07-09 04:43:58 +0700

    slider, footer completed

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col h-screen">
+        <SiteNavBar/>
+        <main>
+            <RouterView />
+        </main>
+        <SiteFooter/>
+    </div>
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import SiteNavBar from './components/SiteNavbar.vue'
+import SiteFooter from './components/SiteFooter.vue'
+
+export default {
+    name: 'App',
+    components: {
+        SiteNavBar,
+        SiteFooter
+    }
+}
+</script>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <SiteSlider />
+    </div>
+</template>
+
+<script>
+import SiteSlider from '../components/home/SiteSlider.vue'
+
+export default {
+    name: 'HomeView',
+    components: {
+        SiteSlider
+    }
+}
+</script>
+
+<style scoped>
+.home {
+    margin: 1%;
+}
+</style>
+
